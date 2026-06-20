d8ece49412f633259cb5fbf2add5f6355075623e
HPmaster <101661188+ComanderCrops@users.noreply.github.com>
2026-06-20 15:31:03 +0200

    ui and math changes

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -5,16 +5,19 @@ public class PlayerCamera : MonoBehaviour
     private PlayerController controller;
     
     public Transform LookAt;
-    public Vector2 MovementRange;
+    public Vector2 RotationBoundaries;
+    // public Vector2 RotationRatio;
     // public float Offset;
     public float mouseSensitivity;
+
+    private Vector2 Rotation;
 
     void Awake() {
         controller = GetComponent<PlayerController>();
     }
 
     void Update() {
-        Vector2 lookDelta = controller.RotationInput;
+        /*Vector2 lookDelta = controller.RotationInput;
 
         float PositionX = LookAt.localPosition.x + lookDelta.x * mouseSensitivity;
         float PositionY = LookAt.localPosition.y + lookDelta.y * mouseSensitivity;
@@ -22,7 +25,12 @@ public class PlayerCamera : MonoBehaviour
         PositionX = Mathf.Clamp(PositionX, -MovementRange.x, MovementRange.x);
         PositionY = Mathf.Clamp(PositionY, -MovementRange.y, MovementRange.y);
 
-        LookAt.localPosition = new Vector3(PositionX, PositionY, LookAt.localPosition.z);
+        LookAt.localPosition = new Vector3(PositionX, PositionY, LookAt.localPosition.z);*/
+
+        Rotation += controller.RotationInput * mouseSensitivity;
+        Rotation = new Vector2(Mathf.Clamp(Rotation.x, -RotationBoundaries.x, RotationBoundaries.x), Mathf.Clamp(Rotation.y, -RotationBoundaries.y, RotationBoundaries.y));
+
+        LookAt.localRotation = Quaternion.Euler(-Rotation.y,Rotation.x,0);
     }
 
     void OnDrawGizmosSelected() {
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,8 +10,9 @@ public class PlayerMovement : MonoBehaviour
 
     public Transform Orientation;
 
-    [Header("Raycast:")]
+    [Header("Raycast & Boxcast:")]
     public Transform KickDirection;
+    public Vector3 boxScale = new Vector3(1f, 1f, 1f);
     public LayerMask LayersToKick;
 
     [Header("Kick:")]
@@ -23,6 +24,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Rotation Handling")]
     public bool thirdPerson;
     public Vector2 spinRandomness = new(5, 10);
+    
     [Header("Speed limits")]
     [Min(0)] public float speedLimit = 0;
 
@@ -31,8 +33,7 @@ public class PlayerMovement : MonoBehaviour
     const float deadzone = 0.2f;
 
     void Awake() {
-        if(Instance)
-        {
+        if(Instance) {
             Debug.LogError("Movement instance already set");
             Destroy(gameObject);
         }
@@ -77,32 +78,52 @@ public class PlayerMovement : MonoBehaviour
         else ProcessJump1Person();
     }
 
-    void ProcessJump1Person()
-    {
+    void ProcessJump1Person() {
         Ray kickRay = new Ray(KickDirection.position, KickDirection.forward);
+
         if(Physics.Raycast(kickRay, out RaycastHit hit, 1000f, LayersToKick)) {
-            Vector3 force = new();
-            // 1. THE FUNNY FORCE
-            // absolute push
-            force += -KickDirection.forward * trueKickForce;
-            // funny push
-            force += new Vector3(hit.normal.x, 0, hit.normal.z).normalized * relativeKickForce;
-
-            rb.AddForce(force, ForceMode.Impulse);
-
-            // 2. THE FUNNY TORQUE
-            // Calculate the rotational axis perpendicular to the kick and the wall normal
-            Vector3 torqueAxis = Vector3.Cross(KickDirection.forward, hit.normal);
-            
-            rb.AddTorque(torqueAxis * torqueForce, ForceMode.Impulse);
+            ForceAndTorque(hit);
         } else {
             // If your not a gamer you dont get to kick like a man
-            rb.AddForce(-KickDirection.forward * nonkickForce);
+            rb.AddForce(-KickDirection.forward * nonkickForce, ForceMode.Impulse);
         }
     }
+    void ProcessJump3Person() {
+        // rb.AddForce(-transform.forward * trueKickForce, ForceMode.Impulse);
+        if(Physics.BoxCast(Orientation.position, boxScale/2, Orientation.forward, out RaycastHit hit, Orientation.rotation, 1000f, LayersToKick)){
+            ForceAndTorque(hit);
+        } else {
+            // If your not a gamer you dont get to kick like a man
+            rb.AddForce(-transform.forward * nonkickForce, ForceMode.Impulse);
+        }
+    }
+    void ForceAndTorque(RaycastHit hit) {
+        Vector3 force = new();
 
-    void ProcessJump3Person()
-    {
-        rb.AddForce(-transform.forward * trueKickForce, ForceMode.Impulse);
+        // 1. THE FUNNY FORCE
+
+        // absolute push
+        force += -KickDirection.forward * trueKickForce;
+        // funny push
+        force += new Vector3(hit.normal.x, 0, hit.normal.z).normalized * relativeKickForce;
+
+        rb.AddForce(force, ForceMode.Impulse);
+
+        // 2. THE FUNNY TORQUE
+
+        // Calculate the rotational axis perpendicular to the kick and the wall normal
+        Vector3 torqueAxis = Vector3.Cross(KickDirection.forward, hit.normal);
+        rb.AddTorque(torqueAxis * torqueForce, ForceMode.Impulse);
+    }
+    
+    private void OnDrawGizmosSelected() {
+        if (Orientation == null) return;
+
+        if (thirdPerson) {
+            Gizmos.color = Color.red;
+            // This draws the starting box position
+            Gizmos.matrix = Matrix4x4.TRS(Orientation.position, Orientation.rotation, Vector3.one);
+            Gizmos.DrawWireCube(Vector3.zero, boxScale);
+        }
     }
 }
