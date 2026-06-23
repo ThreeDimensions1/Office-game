f4f21c3aad7d6b307398f14690db0ccce2fa64d2
Simon Smorada <simonsmorada@gmail.com>
2026-06-23 19:52:18 +0200

    Save System and main menu

--- Assets/Scripts/Ui/Main Menu/PaperButton.cs
+++ Assets/Scripts/Ui/Main Menu/PaperButton.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class PaperButton : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+public class PaperButton : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    public Color onUp, onDown;
+    public bool active = true;
+    public Color onUp, onDown, onHighlight, onInactive;
     public UnityEvent onClick;
 
     SpriteRenderer rend;
@@ -12,20 +13,36 @@ public class PaperButton : MonoBehaviour, IPointerClickHandler, IPointerDownHand
     void Start()
     {
         rend = GetComponent<SpriteRenderer>();
+        rend.color = active ? onUp : onInactive;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        if(active)
         onClick?.Invoke();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        if(active)
         rend.color = onDown;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        if(active)
+        rend.color = onHighlight;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(active)
+        rend.color = onHighlight;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(active)
         rend.color = onUp;
     }
 }
